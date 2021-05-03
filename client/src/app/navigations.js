c4999920bdfea165cec0d3ed43478ce99efea5db
paalamugan <paalamugan@gmail.com>
2021-05-03 19:53:48 +0530

    Update nagivations issue

--- client/src/app/navigations.js
+++ client/src/app/navigations.js
@@ -1,29 +1,28 @@
 import { authRoles } from './auth/authRoles';
 
-export const navigations = [
-  {
-    name: "Users",
-    path: "/users",
-    icon: "people",
-    auth: authRoles.admin
-  },
-  {
-    name: "Posts",
-    path: "/posts",
-    icon: "vertical_split",
-    auth: authRoles.user
-  },
-  {
-    name: "Comments",
-    path: "/comments",
-    icon: "chat",
-    auth: authRoles.admin
-  },
-  {
-    name: "Profile",
-    path: "/profile",
-    icon: "person",
-  }
+export const navigations = [{
+        name: "Users",
+        path: "/users",
+        icon: "people",
+        auth: authRoles.admin
+    },
+    {
+        name: "Posts",
+        path: "/posts",
+        icon: "vertical_split",
+        // auth: authRoles.user
+    },
+    {
+        name: "Comments",
+        path: "/comments",
+        icon: "chat",
+        auth: authRoles.admin
+    },
+    {
+        name: "Profile",
+        path: "/profile",
+        icon: "person",
+    }
 ];
 
 export default navigations;
