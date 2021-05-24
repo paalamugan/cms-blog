5e7548b3da9505bf91243679c77a2aae6279965e
Paalamugan <paalamugan@atatus.com>
2021-05-24 11:07:16 +0530

    Fixed oauth route issue

--- client/src/app/auth/Auth.jsx
+++ client/src/app/auth/Auth.jsx
@@ -3,13 +3,13 @@ import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { setSessionData } from "../redux/actions/SessionActions";
-import { getNavigationByUser } from "../redux/actions/NavigationAction";
+import { refreshNavigationByUser } from "../redux/actions/NavigationAction";
 import jwtAuthService from "../services/jwtAuthService";
 import localStorageService from "../services/localStorageService";
 import history from "history.js";
 import { unAuthRoutes } from "app/constant";
 
-const checkJwtAuth = async (setSessionData, getNavigationByUser) => {
+const checkJwtAuth = async (setSessionData, refreshNavigationByUser) => {
   // You need to send token to your server to check token is valid
   // modify loginWithToken method in jwtService
   let pathname = history.location.pathname;
@@ -25,7 +25,7 @@ const checkJwtAuth = async (setSessionData, getNavigationByUser) => {
   if (session) {
 
     setSessionData(session);
-    getNavigationByUser();
+    refreshNavigationByUser(session);
 
     setTimeout(() => {
       if (!session.verified) {
@@ -46,22 +46,22 @@ const checkJwtAuth = async (setSessionData, getNavigationByUser) => {
 
 };
 
-const Auth = ({ children, setSessionData, getNavigationByUser }) => {
+const Auth = ({ children, setSessionData, refreshNavigationByUser }) => {
   setSessionData(localStorageService.getUser());
 
   useEffect(() => {
-    checkJwtAuth(setSessionData, getNavigationByUser);
-  }, [setSessionData, getNavigationByUser]);
+    checkJwtAuth(setSessionData, refreshNavigationByUser);
+  }, [setSessionData, refreshNavigationByUser]);
 
   return <Fragment>{children}</Fragment>;
 };
 
 const mapStateToProps = state => ({
   setSessionData: PropTypes.func.isRequired,
-  getNavigationByUser: PropTypes.func.isRequired,
+  refreshNavigationByUser: PropTypes.func.isRequired,
   login: state.login
 });
 
-export default connect(mapStateToProps, { setSessionData, getNavigationByUser })(
+export default connect(mapStateToProps, { setSessionData, refreshNavigationByUser })(
   Auth
 );
