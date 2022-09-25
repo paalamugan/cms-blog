99cecb940a5eea44e5c98e422527c45af745aba0
paalamugan <paalamugan@gmail.com>
2022-09-26 00:29:40 +0530

    fix: no-data image loaded

--- client/src/blog/components/BlogNoData.jsx
+++ client/src/blog/components/BlogNoData.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Card, Grid } from "@material-ui/core";
 
 const BlogNoData = ({ children, className, message }) => {
