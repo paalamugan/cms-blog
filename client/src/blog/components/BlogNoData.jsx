8928e6d3e36b611808c4a2a0b84bc7c6fcb226fd
paalamugan <paalamugan@gmail.com>
2022-09-26 00:22:27 +0530

    fix: no-data image loaded

--- client/src/blog/components/BlogNoData.jsx
+++ client/src/blog/components/BlogNoData.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Grid } from "@material-ui/core";
 
 const BlogNoData = ({ children, className, message }) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   return (
     <Grid
       container
@@ -14,8 +15,10 @@ const BlogNoData = ({ children, className, message }) => {
 
       <Grid item xs={10} sm={8} md={6} lg={4} xl={3}>
         <Card className="my-10">
-          <Card elevation={0} className="upgrade-card bg-light-primary p-sm-24">
-            <img src="/assets/images/illustrations/no-data.png" alt="no-data" />
+          <Card elevation={0} className={isImageLoaded ? 'upgrade-card bg-light-primary p-sm-24' : 'display-none'}>
+            <img src="/assets/images/illustrations/no-data.png" alt="no-data" onLoad={() => {
+              setIsImageLoaded(true);
+            }} />
             {children ? children : (
               <h4 className="mt-10">{ message || 'No result found.'}</h4>
             )}
